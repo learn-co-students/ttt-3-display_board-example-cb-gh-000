ecfdbffc360d7e04d4c397ec3d223ad39ad686bc
7s Dream <7seconddream@gmail.com>
2017-01-19 17:36:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  tone = [(["   "] * 3).join("|")] * 3
+  puts tone.join("\n" + ((["-"] * 11)).join("") + "\n")
+end
