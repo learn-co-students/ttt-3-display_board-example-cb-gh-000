b538e85aa39f4843c519b44989bdaf780967436d
Sanatt Abrol <sanatt.abrol.in@gmail.com>
2017-06-16 20:43:50 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,17 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def print_row
+  2.times {print "   |"}
+  puts "   "
+end
+
+def display_board
+  print_row
+  puts "-----------"
+  print_row
+  puts "-----------"
+  print_row
+end
+
+
+
+  
