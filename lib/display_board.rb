6d4f905c24afdb64d813ab73e5f39a06d15ef9ad
Suvendu Barik <sb749@snu.edu.in>
2017-07-14 11:16:00 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,18 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  val = "   "
+  for i in 0...3
+    line = ""
+    for j in 0..2
+      if j!=2
+          line += val+"|"
+      else
+          line += val
+      end
+    end
+    puts line
+    if i != 2
+      puts "-----------"
+    end
+  end
+end
