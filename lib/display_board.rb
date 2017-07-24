83fb6901cb38946e5d4398c5a1e3030ab71ae95f
Dao Minh <1312359@student.hcmus.edu.vn>
2017-07-24 17:19:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,16 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board (n = 3) # width = heighh of square board
   board = Array.new(n*n, "   ")
+  result = Array.new
   n.times do |i|
-    (n-1).times do |j|
-      print board[n*i + j] + "|"
+    temp = Array.new
+    n.times do |j|
+      temp << board[n*i + j]
     end
-    puts board[n*i + n-1] #last element of row
-    puts "-"*11 if i != n-1
+    result << temp.join("|") + "\n"
+    temp.clear
+    #puts board[n*i + n-1] #last element of row
+    #puts "-"*11 if i != n-1
   end
+  puts result.join("-"*11+"\n")
 end
