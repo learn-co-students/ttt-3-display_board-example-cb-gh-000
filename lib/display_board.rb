f00bbba0a69eab3d39d403b5f6f41afa01948ffb
Omar Ali <omarmaboelenen@gmail.com>
2017-08-27 15:05:56 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  for j in 0...3 do
-    if j > 0
-      11.times { print "-" }
-      puts
-    end
+  for j in 0...3
+    puts "-----------" if j > 0
     for i in 0...3
-      if i > 0
-        print "|"
-      end
-      print "   "
+       if i > 0 then print "|   " else print "   " end 
     end
     puts
   end
