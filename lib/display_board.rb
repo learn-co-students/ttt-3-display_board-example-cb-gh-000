196b7941c3b7a6c90caf5f709a690c7512cc900e
Fernando Peña <ramses.penarj@uanl.edu.mx>
2017-11-04 00:34:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  col = "   |   |   "
+  separator = "-----------"
+  puts col
+  puts separator
+  puts col
+  puts separator
+  puts col
+end
