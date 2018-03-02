0b5d2afce49ba316952f853b97bdc218d14ddaa5
Suraj Patel <snpatel2@sheffield.ac.uk>
2018-03-02 13:46:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  r1 = "   |   |   "
+  r2 = "-----------"
+  r3 = "   |   |   "
+  r4 = "-----------"
+  r5 = "   |   |   "
+  puts r1
+  puts r2
+  puts r3
+  puts r4
+  puts r5
+end
