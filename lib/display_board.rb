86e72d3faa26f72669f3b26e70cc1d4795615420
Ayush Rawal <ayushrawal12@gmail.com>
2018-05-11 12:52:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def print_dashes
+  puts "-"*11
+end
+def display_board
+  board = Array.new(3, "   ")
+  puts board.join("|")
+  print_dashes
+  puts board.join("|")
+  print_dashes
+  puts board.join("|")
+end
+display_board
