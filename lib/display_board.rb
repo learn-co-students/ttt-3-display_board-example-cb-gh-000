dfd0432efc0318e159b06adf45b65f56e50bca5c
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-21 00:59:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,4 +7,21 @@ puts "-----------"
 puts "   |   |   "
 end
 
-display_board
+
+def valid_move?(board,index)
+if(index >= 0 && index <= 8 && position_taken?(board,index) == false)
+true
+end
+end
+
+def position_taken?(board, index)
+if(board[index] == "" || board[index] == " " || board[index] == nil)
+false
+elsif board[index] == "X" || board[index] == "O"
+true
+end
+end
+
+def move(board,index, token = 'X')
+  board[index] = token
+end
