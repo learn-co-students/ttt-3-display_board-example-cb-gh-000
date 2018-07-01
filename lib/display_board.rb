5ddc260bc3a3402d83ff697d1ab83e743289e0f9
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-01 19:10:54 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,70 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+    array = [" ", "|", "-"]
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print array[1]
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print array[1]
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print "\n"
+
+    for i in 1..11
+    print array[2]
+    end
+
+    print "\n"
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print array[1]
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print array[1]
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print "\n"
+
+    for i in 1..11
+    print array[2]
+    end
+
+    print "\n"
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print array[1]
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print array[1]
+
+    for i in 1..3
+    print array[0]
+    end
+
+    print "\n"
+end
