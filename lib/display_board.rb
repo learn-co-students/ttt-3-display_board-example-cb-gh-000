abcdad12e7f54595a602b4ef8f451c4879db80e7
Nyior Clement <nyior.clement@aun.edu.ng>
2019-10-24 09:00:38 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  array = ["   |   |   ", "-----------"]
+  puts array[0]
+  puts array[1]
+  puts array[0]
+  puts array[1]
+  puts array[0]
+
+end
+
+display_board
