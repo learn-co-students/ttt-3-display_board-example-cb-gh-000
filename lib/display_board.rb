fc44a60fdf7b2850f5349a34717586e300a5433e
Erro Remaider Ashivudhi <pandenierro@gmail.com>
2020-04-12 18:10:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+# 3.times{puts " "," "," "}
+welcome = "welcome to tic tac toe game"
+welcome.upcase
+puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
